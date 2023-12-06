c41d99148e36c593b241ff50e817d4e1d756ea3a
Byekibe <kibetpeter95@gmail.com>
2023-12-06 04:10:16 +0300

    Falsk app

--- server.py
+++ server.py
@@ -2,24 +2,26 @@ from flask import Flask
 from dotenv import load_dotenv
 load_dotenv()
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 port = os.getenv("PORT")
 
 
-@app.route("/")
+@app.route("/api")
 def home():
     names = ["Kia", "Kai", "Koa", "Kei", "Kui"]
     ages = [12, 10, 15, 9, 5]
-    height = (1.5, 1.2, 1.7, 1, 0.7)
+    heights = (1.5, 1.2, 1.7, 1, 0.7)
     return {
         "names": names,
         "ages": ages,
-        "height": height
+        "heights": heights
     }, 200
 
-@app.route("/about")
+@app.route("/api/about")
 def about():
     str = "A simple Flask App"
     return str, 200
